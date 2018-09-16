c71eb674a2dae09c0aee790f640db059389d10c5
Emilia Cristea <emilia.cristea@gmail.com>
2018-09-16 20:27:29 +0300

    Fixes style in Text component

--- app/components/Text.js
+++ app/components/Text.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text } from 'react-native';
 
-const NormalText = ({ children, style }) => <Text style={[styles.text, style]}>{children}</Text>;
-const BoldText = ({ children, style }) => <Text style={[styles.text, styles.bold, style]}>{children}</Text>;
+const NormalText = ({ children, style }) => <Text style={[styles.normal, style]}>{children}</Text>;
+const BoldText = ({ children, style }) => <Text style={[styles.normal, styles.bold, style]}>{children}</Text>;
 
 const styles = StyleSheet.create({
   normal: {
-    fontSize: 20,
+    fontSize: 18,
     color: 'black'
   },
   bold: {
