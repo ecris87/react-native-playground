e0a0f0ceea235f8cafb532c576de148b699ad387
Emilia Cristea <emilia.cristea@gmail.com>
2018-09-17 17:48:41 +0300

    Adds new text components

--- app/components/Text.js
+++ app/components/Text.js
@@ -3,15 +3,27 @@ import { StyleSheet, Text } from 'react-native';
 
 const NormalText = ({ children, style }) => <Text style={[styles.normal, style]}>{children}</Text>;
 const BoldText = ({ children, style }) => <Text style={[styles.normal, styles.bold, style]}>{children}</Text>;
+const ErrorText = ({ children, style }) => <Text style={[styles.normal, styles.error, style]}>{children}</Text>;
+const SuccessText = ({ children, style }) => <Text style={[styles.normal, styles.success, style]}>{children}</Text>;
+const SmallText = ({ children, style }) => <Text style={[styles.normal, styles.small, style]}>{children}</Text>;
 
 const styles = StyleSheet.create({
   normal: {
     fontSize: 18,
     color: 'black'
   },
+  small: {
+    fontSize: 14
+  },
   bold: {
     fontWeight: 'bold'
+  },
+  error: {
+    color: 'red'
+  },
+  success: {
+    color: 'green'
   }
 });
 
-export { NormalText, BoldText };
+export { NormalText, SmallText, BoldText, ErrorText, SuccessText };
